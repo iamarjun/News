b8b3a53f72060de14959004793bfa00ee38308b3
iamarjun <arjunmanoj1995@gmail.com>
2020-10-17 17:45:31 +0530

    convert theme to material theme

--- app/src/main/java/com/example/news/MainActivity.kt
+++ app/src/main/java/com/example/news/MainActivity.kt
@@ -2,11 +2,7 @@ package com.example.news
 
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.viewpager.widget.ViewPager
 import com.example.news.databinding.ActivityMainBinding
-import com.example.news.util.showToast
-import com.google.android.material.tabs.TabLayout
-import com.momentsnap.android.EventObserver
 
 class MainActivity : BaseActivity() {
 
@@ -14,8 +10,6 @@ class MainActivity : BaseActivity() {
 
     private lateinit var adapter: SectionsPagerAdapter
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewPager: ViewPager
-    private lateinit var tabs: TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -24,9 +18,6 @@ class MainActivity : BaseActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
-
-        viewPager = binding.viewPager
-        tabs = binding.tabs
 
 //        viewModel.newsSource.observe(this, EventObserver {
 //            it.sources?.let {
